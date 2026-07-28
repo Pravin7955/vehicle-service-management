fc0bdb58c127f5bbdd650cd9d47f369e767486f6
Pravin Panchal <pravinpanchal7955@gmail.com>
2026-07-28 13:59:55 +0530

    Add computed total cost field

--- addons/vehicle_service/models/vehicle.py
+++ addons/vehicle_service/models/vehicle.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class VehicleVehicle(models.Model):
@@ -34,3 +34,25 @@ class VehicleVehicle(models.Model):
         string="Active",
         default=True,
     )
+
+    parts_cost = fields.Float(
+        string="Parts Cost",
+    )
+
+    labour_cost = fields.Float(
+        string="Labour Cost",
+    )
+
+    total_cost = fields.Float(
+        string="Total Cost",
+        compute="_compute_total_cost",
+        store=True,
+    )
+
+    @api.depends("parts_cost", "labour_cost")
+    def _compute_total_cost(self):
+        for record in self:
+            record.total_cost = (
+                record.parts_cost +
+                record.labour_cost
+            )
